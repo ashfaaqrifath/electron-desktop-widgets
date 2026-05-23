b33d1d0b8b1f5c206fb86ceee0df8512f1267fd7
Ashfaaq Rifath <ashfaaq.rifath2@gmail.com>
2026-05-23 19:12:17 +0530

    Delete preload.js

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  getBatteryStats: () => ipcRenderer.invoke('battery:get-stats'),
-  closeWindow: () => ipcRenderer.invoke('app:close'),
-});
